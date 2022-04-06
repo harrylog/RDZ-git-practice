0117a8b1ad73cc613971058c3aa435e4396724c0
harry.log <harry@infinitylabs.co.il>
2022-04-06 09:49:28 +0300

    instructions

--- hello.js
+++ hello.js
@@ -1,1 +1,19 @@
 console.log("my name is harry");
+
+console.log('do the following');
+
+console.log(' change and push  ');
+
+console.log(' let someone change and push - now u try to change and push without pulling  ');
+
+
+console.log(' handle the conflict   ');
+
+
+console.log(' branch out amd merge to main branch ');
+
+console.log(' use commands like git log --oneline    ');
+
+console.log(' use commands like git revert -- hard soft - mind the diffs   ');
+
+
