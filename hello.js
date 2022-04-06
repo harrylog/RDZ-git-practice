16f5615e48dcdeb0d71a344ebcd7acef717df757
harry.log <harry@infinitylabs.co.il>
2022-04-06 10:20:02 +0300

    ops

--- hello.js
+++ hello.js
@@ -1,3 +1,18 @@
 console.log("my name is harry");
 
+console.log("my name is harry");
+
+
+console.log("my name is harry");
+
+
+console.log("my name is harry");
+
+
+console.log("my name is harry");
+
+
+console.log("my name is harry");
+
+
 
