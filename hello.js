777065dc8f815aea975d2097396c4686ec9453e2
harry.log <harry@infinitylabs.co.il>
2022-04-06 10:00:41 +0300

    ops

--- hello.js
+++ hello.js
@@ -1,25 +1,3 @@
 console.log("my name is harry");
 
-console.log("do the following");
 
-console.log(" change and push  ");
-
-console.log(
-  " let someone change and push - now u try to change and push without pulling  "
-);
-
-console.log(" handle the conflict   ");
-
-console.log(" branch out amd merge to main branch ");
-
-console.log(" use commands like git log --oneline    ");
-
-console.log(" use commands like git revert -- hard soft - mind the diffs   ");
-
-const harrysFunc = () => {
-  let a = 6;
-  let b = 4;
-  let sum = a + b;
-
-  return sum
-};
